b529c6788430656a25dcba0de9a9a31e3adaedb1
romeocovaci <romeocovaci@gmail.com>
2018-12-03 22:26:02 +0200

    Update plugin to 1.0.6

---
+++ CHANGELOG.md
@@ -0,0 +1,43 @@
+# Changelog
+======
+1.0.6
+======
+- NEW:	Added total count of subscribers
+- NEW:	Use feature image / product image instead of default notification icon
+- NEW:	Remove failed subscribers who have blocked notifications (Settings > General)
+- FIX:	Notifications in backend ordered by date
+
+======
+1.0.5
+======
+- NEW:	Split large ids into 400 chunk notifications
+
+======
+1.0.4
+======
+- NEW:	Muliple CURL support
+- FIX:	Performance tweak when too many user ids
+
+======
+1.0.3
+======
+- FIX:	Notficiations not send out when user roles were
+		empty in backend
+- FIX: 	Trashed posts notifications
+
+======
+1.0.2
+======
+- NEW:	Up to 4 Custom WP Notifications 
+- FIX:	Updated WPML Keys
+- FIX:	Small Bug fixes
+
+======
+1.0.1
+======
+- FIX:	Small Bug fixes
+
+======
+1.0.0
+======
+- Inital release
---
+++ wordpress-fire-push.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ *
+ * @link              https://github.com/hypericumimpex/hyp-push/
+ * @since             1.0.0
+ * @package           WordPress_Fire_Push
+ *
+ * @wordpress-plugin
+ * Plugin Name:       HYP Push
+ * Plugin URI:        https://github.com/hypericumimpex/hyp-push/
+ * Description:       Hypericum Firebase Web Push Notificari...  
+ * Version:           1.0.6
+ * Author:            Romeo C.
+ * Author URI:        https://github.com/hypericumimpex/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wordpress-fire-push
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-wordpress-fire-push-activator.php
+ */
+function activate_WordPress_Fire_Push() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wordpress-fire-push-activator.php';
+	$activator = new WordPress_Fire_Push_Activator();
+	$activator->activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-wordpress-fire-push-deactivator.php
+ */
+function deactivate_WordPress_Fire_Push() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wordpress-fire-push-deactivator.php';
+	WordPress_Fire_Push_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_WordPress_Fire_Push' );
+register_deactivation_hook( __FILE__, 'deactivate_WordPress_Fire_Push' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-wordpress-fire-push.php';
+
+/**
+ * Run the Plugin
+ * @author Romeo C.
+ * @version 1.0.0
+ * @since   1.0.0
+ * @link    https://github.com/hypericumimpex/hyp-push/
+ */
+function run_WordPress_Fire_Push() {
+
+	$plugin_data = get_plugin_data( __FILE__ );
+	$version = $plugin_data['Version'];
+
+	$plugin = new WordPress_Fire_Push($version);
+	$plugin->run();
+
+}
+
+include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+if ( is_plugin_active('redux-framework/redux-framework.php')){
+	run_WordPress_Fire_Push();
+} else {
+	add_action( 'admin_notices', 'run_WordPress_Fire_Push_Not_Installed' );
+}
+
+function run_WordPress_Fire_Push_Not_Installed()
+{
+	?>
+    <div class="error">
+      <p><?php _e( 'WordPress Fire_Push requires the Redux Framework Please install or activate it before!', 'wordpress-fire-push'); ?></p>
+    </div>
+    <?php
+}
